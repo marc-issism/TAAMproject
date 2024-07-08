c3f5becd0b905d740e0d4082ad365403afb54ab1
PizzaAuFromage <moojycua@gmail.com>
2024-07-08 14:20:07 -0400

    fixed fragment linking

--- app/src/main/java/com/example/taam_project/MainActivity.java
+++ app/src/main/java/com/example/taam_project/MainActivity.java
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -26,8 +28,17 @@ public class MainActivity extends AppCompatActivity {
         DatabaseReference dbRef = db.getReference("IDK WHAT GOES HERE");
 
         // Change this for default activity/fragment
-       //if(savedInstanceState == null){
-       //     loadFragment(new HomeFragment());
-      // }
+       if(savedInstanceState == null){
+            loadFragment(new SearchFragment());
+       }
+
+
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }

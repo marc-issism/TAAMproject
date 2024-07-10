2515eaa9b50a517327dad18f236dc93839453dc5
Albert Huynh <itsalbert.huynh@mail.utoronto.ca>
2024-07-09 23:21:58 -0400

    feature: added AddItem fragment

---
+++ app/src/main/java/com/example/taam_project/AddItem.java
@@ -0,0 +1,24 @@
+package com.example.taam_project;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+public class AddItem extends Fragment {
+    private EditText lotNumber, name, category, period, description;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_add_item, container, false);
+        lotNumber = view.findViewById()
+        return view;
+    }
+
+}

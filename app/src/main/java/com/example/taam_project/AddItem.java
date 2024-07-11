61575cc7ba12ae524852f9aeb2977737660e145e
Albert Huynh <itsalbert.huynh@mail.utoronto.ca>
2024-07-11 17:17:41 -0400

    feature: fixed xml layout

--- app/src/main/java/com/example/taam_project/AddItem.java
+++ app/src/main/java/com/example/taam_project/AddItem.java
@@ -8,16 +8,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 public class AddItem extends Fragment {
-    private EditText lotNumber, name, category, period, description;
+    private EditText lotNumber, name, description;
+    private Spinner category, period;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_add_item, container, false);
-        lotNumber = view.findViewById()
+        lotNumber = view.findViewById(R.id.EditLotNumber);
+        name = view.findViewById(R.id.EditName);
+        category = view.findViewById(R.id.EditCategory);
+        period = view.findViewById(R.id.EditPeriod);
+        description = view.findViewById(R.id.EditDescription);
+
         return view;
     }
 
--- app/src/main/java/com/example/taam_project/MainActivity.java
+++ app/src/main/java/com/example/taam_project/MainActivity.java
@@ -7,8 +7,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -23,19 +21,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Database linking
-        String dbURL = "https://cscb07-taam-default-rtdb.firebaseio.com/";
+        String dbURL = "";
         db = FirebaseDatabase.getInstance(dbURL);
       //  DatabaseReference dbRef = db.getReference("IDK WHAT GOES HERE");
         //
-       if(savedInstanceState == null){
-            loadFragment(new HomeFragment());
-       }
-    }
-    private void loadFragment(Fragment fragment){
-        FragmentTransaction frag =getSupportFragmentManager().beginTransaction();
-        frag.replace(R.id.fragment_container, fragment);
-        frag.addToBackStack(null);
-        frag.commit();
-
+       //if(savedInstanceState == null){
+       //     loadFragment(new HomeFragment());
+      // }
     }
 }

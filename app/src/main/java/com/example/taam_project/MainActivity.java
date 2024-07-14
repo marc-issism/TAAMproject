711c800ba10d2744502e1dd4b17c13cb9830d2b6
patrick-hu7 <patrickhuuu7@gmail.com>
2024-07-14 14:36:23 -0400

    feat: add simple popup

--- app/src/main/java/com/example/taam_project/MainActivity.java
+++ app/src/main/java/com/example/taam_project/MainActivity.java
@@ -4,15 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
     Button ReportButton;
@@ -26,25 +24,20 @@ public class MainActivity extends AppCompatActivity {
 
         // Database linking
         String dbURL = "";
-        db = FirebaseDatabase.getInstance(dbURL);
-      //  DatabaseReference dbRef = db.getReference("IDK WHAT GOES HERE");
-        //
+        // db = FirebaseDatabase.getInstance(dbURL);
+        // DatabaseReference dbRef = db.getReference("IDK WHAT GOES HERE");
 
-       //if(savedInstanceState == null){
-       //     loadFragment(new HomeFragment());
-      // }
-      
-       if(savedInstanceState == null){
-            loadFragment(new HomeFragment());
+       if(savedInstanceState == null) {
+           loadFragment(new HomeFragment());
        }
     }
+
     private void loadFragment(Fragment fragment){
         FragmentTransaction frag =getSupportFragmentManager().beginTransaction();
         frag.replace(R.id.fragment_container, fragment);
         frag.addToBackStack(null);
         frag.commit();
-  
-      
+
         /*teddy's code (will move to appropriate position later if required)*/
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -59,6 +52,12 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this, ReportActivity.class);
                 startActivity(intent);
             }
+        });
+
+        // Patrick - Admin Login Button
+        findViewById(R.id.show_login_button).setOnClickListener(v-> {
+            AdminLoginFragment loginFrag = new AdminLoginFragment();
+            loginFrag.show(getSupportFragmentManager(), "AdminLoginFragment");
         });
     }
 }

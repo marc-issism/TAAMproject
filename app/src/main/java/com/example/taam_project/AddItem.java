e21ad8e70977ddc2a5f7ee0c596c85d36a121b95
Albert Huynh <itsalbert.huynh@mail.utoronto.ca>
2024-07-22 22:51:18 -0400

    fix: fixed async. media uploading time to firebase storage

--- app/src/main/java/com/example/taam_project/AddItem.java
+++ app/src/main/java/com/example/taam_project/AddItem.java
@@ -1,5 +1,7 @@
 package com.example.taam_project;
 
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -22,6 +24,7 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.internal.Storage;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 
@@ -29,7 +32,10 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageMetadata;
 import com.google.firebase.storage.StorageReference;
+
+import java.util.concurrent.TimeUnit;
 
 
 public class AddItem extends Fragment {
@@ -40,7 +46,6 @@ public class AddItem extends Fragment {
     private StorageReference sb;
     ActivityResultLauncher<Intent> resultLauncher;
     private Uri image;
-
 
 
     @Override
@@ -108,6 +113,7 @@ public class AddItem extends Fragment {
         }
 
         itemsRef = db.getReference("test/");
+
        // String id = itemsRef.push().getKey();
 
         uploadImage(tmpLot);
@@ -115,8 +121,17 @@ public class AddItem extends Fragment {
         Item item = new Item(tmpLot, tmpName, tmpCategory, tmpPeriod, tmpDisc, "");
         itemsRef.child(tmpLot).setValue(item).addOnCompleteListener(task -> {
             if (task.isSuccessful()) {
-                getMediaLink(tmpLot);
-                Toast.makeText(getContext(), "Successfully added", Toast.LENGTH_SHORT).show();
+                try {
+                    getMediaLink(tmpLot, 0);
+                    setMediaType(tmpLot, 0);
+                    Toast.makeText(getContext(), "Item added, waiting for media...", Toast.LENGTH_LONG).show();
+
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+
+
             } else {
                 Toast.makeText(getContext(), "Failed to add item", Toast.LENGTH_SHORT).show();
             }
@@ -139,7 +154,7 @@ public class AddItem extends Fragment {
                             image = result.getData().getData();
                         }
                         catch (Exception e){
-                            Toast.makeText(getContext(), "Something went wrong", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(getContext(), "Could not register image", Toast.LENGTH_SHORT).show();
 
                         }
                     }
@@ -155,19 +170,59 @@ public class AddItem extends Fragment {
         imageRef.putFile(image);
     }
 
-    private void getMediaLink(String tmpLot){
-        sb.child(tmpLot).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+    private void getMediaLink(String lotNum, Integer loop) throws InterruptedException {
+        if (loop == 10) {
+            Toast.makeText(getContext(), "Media could not be uploaded", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        sb.child(lotNum).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
             @Override
             public void onSuccess(Uri uri) {
                 String link = uri.toString();
-                itemsRef.child(tmpLot).child("media").setValue(link);
-                }
+                itemsRef.child(lotNum).child("media").setValue(link);
+                Toast.makeText(getContext(), "Media successfully uploaded", Toast.LENGTH_SHORT).show();
+
+            }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception exception) {
-
-                Toast.makeText(getContext(), "Failed to process image", Toast.LENGTH_SHORT).show();
+                try {
+                    TimeUnit.SECONDS.sleep(2);
+                    getMediaLink(lotNum, loop+1);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             }
         });
+    }
+    private void setMediaType(String lot, Integer loop){
+        if (loop == 10){
+            Toast.makeText(getContext(), "Media could not be uploaded", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        StorageReference item = sb.child(lot);
+        item.getMetadata().addOnSuccessListener(new OnSuccessListener<StorageMetadata>() {
+            @Override
+            public void onSuccess(StorageMetadata storageMetadata) {
+                // Metadata now contains the metadata for 'images/forest.jpg'
+                String mediaType = storageMetadata.getContentType();
+                itemsRef.child(lot).child("mediaType").setValue(mediaType);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                try {
+                    TimeUnit.SECONDS.sleep(2);
+                    setMediaType(lot, loop+1);
+
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+
     }
 }

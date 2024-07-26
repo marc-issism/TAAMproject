e4588615f556750c8199d3c4a5a2d08d14724802
patrick-hu7 <patrickhuuu7@gmail.com>
2024-07-26 16:58:51 -0400

    feat: only admin users can create other admin accounts

--- app/src/main/java/com/example/taam_project/AdminLoginFragment.java
+++ app/src/main/java/com/example/taam_project/AdminLoginFragment.java
@@ -1,6 +1,5 @@
 package com.example.taam_project;
 
-import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -16,19 +15,12 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.AuthResult;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import androidx.annotation.NonNull;
+
 import android.util.Log;
 
-import java.util.concurrent.Executor;
-
-
-/*
-The pop-up login screen.
- */
 public class AdminLoginFragment extends DialogFragment {
     private static final String TAG = "AdminLoginFragment";
     private FirebaseAuth mAuth;
@@ -47,8 +39,7 @@ public class AdminLoginFragment extends DialogFragment {
 
         EditText emailEditText = view.findViewById(R.id.email_edit_text);
         EditText passwordEditText = view.findViewById(R.id.password_edit_text);
-        Button loginButton = view.findViewById(R.id.login_button);
-        Button createAccountButton = view.findViewById(R.id.create_account_button);
+        Button loginButton = view.findViewById(R.id.logout_button);
         Button closeButton = view.findViewById(R.id.close_button);
 
         mAuth = FirebaseAuth.getInstance();
@@ -61,17 +52,6 @@ public class AdminLoginFragment extends DialogFragment {
                 Toast.makeText(getActivity(), "Please enter both username and password", Toast.LENGTH_SHORT).show();
             } else {
                 login(username, password);
-            }
-        });
-
-        createAccountButton.setOnClickListener(v -> {
-            String username = emailEditText.getText().toString().trim();
-            String password = passwordEditText.getText().toString().trim();
-
-            if (username.isEmpty() || password.isEmpty()) {
-                Toast.makeText(getActivity(), "Please enter both username and password", Toast.LENGTH_SHORT).show();
-            } else {
-                createAccount(username, password);
             }
         });
 
@@ -105,49 +85,10 @@ public class AdminLoginFragment extends DialogFragment {
                             }
                         }
                     } else {
-
                         // If sign in fails, display a message to the user.
                         Log.w(TAG, "signInWithEmail:failure", task.getException());
-                        Toast.makeText(getActivity(), "Authentication failed." + task.getException().getMessage(),
+                        Toast.makeText(getActivity(), task.getException().getMessage(),
                                 Toast.LENGTH_SHORT).show();
-                    }
-                }
-            });
-    }
-
-    private void createAccount(String email, String password) {
-        mAuth.createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-                    if (task.isSuccessful()) {
-                        // Account creation success
-                        FirebaseUser user = mAuth.getCurrentUser();
-                        if (user != null) {
-                            // Send verification email
-                            user.sendEmailVerification()
-                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                        @Override
-                                        public void onComplete(@NonNull Task<Void> task) {
-                                            if (task.isSuccessful()) {
-                                                Log.d(TAG, "Verification email sent.");
-                                                Toast.makeText(getActivity(), "Account successfully created. Please check your email to verify your account.",
-                                                        Toast.LENGTH_SHORT).show();
-                                            } else {
-                                                Log.e(TAG, "Failed to send verification email.", task.getException());
-                                                Toast.makeText(getActivity(), "Failed to send verification email.",
-                                                        Toast.LENGTH_SHORT).show();
-                                            }
-                                        }
-                                    });
-                        }
-                    } else {
-                        // Account creation failed
-                        Exception exception = task.getException();
-                        if (exception != null) {
-                            Log.w(TAG, "createUserWithEmail:failure", exception);
-                            Toast.makeText(getActivity(), "Authentication failed. " + exception.getMessage(), Toast.LENGTH_SHORT).show();
-                        }
                     }
                 }
             });
@@ -180,13 +121,3 @@ public class AdminLoginFragment extends DialogFragment {
         }
     }
 }
-
-// To check whether a user is already logged in:
-//public void onStart() {
-//    super.onStart();
-//    // Check if user is signed in (non-null) and update UI accordingly.
-//    FirebaseUser currentUser = mAuth.getCurrentUser();
-//    if(currentUser != null){
-//        reload(); // or do something else
-//    }
-//}
